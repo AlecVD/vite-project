eecec17f79a8e5fa16d6c40ec86feb636185998e
alecvd <14879968+AlecVD@users.noreply.github.com>
2023-06-01 14:58:28 -0700

    add sizes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import * as THREE from 'three';
-
+import * as THREE from 'three'
+import '/style.css'
 //scene
 const scene = new THREE.Scene()
 
-//sphere
+//sphere 
 const geometry = new THREE.SphereGeometry(4,64,64);
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff84",
@@ -11,12 +11,22 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry,material)
 scene.add(mesh)
 
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
 //camera
-const camera = new THREE.PerspectiveCamera(50,800,600)
+const camera = new THREE.PerspectiveCamera(50,sizes.width/sizes.height)
+camera.position.z = 20
 scene.add(camera)
+
+//light
+const light = new THREE.PointLight(0xffffff,1,100)
+light.position.set(0,10,10)
+scene.add(light)
 
 //render
 const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({canvas})
-renderer.setSize(800,600)
-renderer.render(scene,camera)
+renderer.setSize(sizes.width,sizes.height)
+renderer.render(scene,camera) 
